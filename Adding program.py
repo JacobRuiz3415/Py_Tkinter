9077885eec1894d415d5461a8fb098d0fc21ad6f
JacobRuiz3415 <jacobruiz428@gmail.com>
2026-08-12 14:10:26 -0700

    complete data entry

--- Adding program.py
+++ Adding program.py
@@ -2,19 +2,19 @@ import tkinter as tk
 
 def retrieve_vardata():
 
-    number1 = entry1.get()
+    number1 = int(entry1.get())
     print(number1)
 
-    number2 = entry2.get()
+    number2 = int(entry2.get())
     print(number2)
     print(f'sum of {number1} and {number2} is {number1 + number2}')
 
 root = tk.Tk()
 
-root.title("adding number")
+root.title("Adding Numbers")
 
 tk.Label(root, text="First Number:").grid(row= 0, column= 0)
-tk.Label(root, text="Second Number:").grid(row= 1, column= 1)
+tk.Label(root, text="Second Number:").grid(row= 1, column= 0)
 
 entry1 = tk.Entry(root)
 entry2 = tk.Entry(root)
@@ -23,6 +23,6 @@ entry1.grid(row=0, column= 1)
 entry2.grid(row=1, column= 1)
 
 button = tk.Button(root, text = "Submit", command= retrieve_vardata)
-button.grid(row=4, column=1)
+button.grid(row=3, column=1)
 
 root.mainloop()
