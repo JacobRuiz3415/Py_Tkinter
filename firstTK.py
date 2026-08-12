4784fb2e25e81942211970fee487d48ecb1c0068
JacobRuiz3415 <jacobruiz428@gmail.com>
2026-08-12 11:57:40 -0700

    ok

--- firstTK.py
+++ firstTK.py
@@ -1,10 +1,10 @@
 import tkinter as tk
-
+#used to create the main window
 root = tk.Tk()
 # Widgets are added here
 
 root.title("First widget")
 
-label = tk.Label(root, text="My program")
+label = tk.Label(root, text="My Program is being displayed here")
 label.pack()
 root.mainloop()
