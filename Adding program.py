b033522ded0f76bbdb9e681fbd4419c8a2351d47
JacobRuiz3415 <jacobruiz428@gmail.com>
2026-08-12 13:46:19 -0700

    added a button

--- Adding program.py
+++ Adding program.py
@@ -2,6 +2,8 @@ import tkinter as tk
 
 root = tk.Tk()
 
+root.title("adding number")
+
 tk.Label(root, text="First Number:").grid(row= 0, column= 0)
 tk.Label(root, text="Second Number:").grid(row= 1, column= 1)
 
@@ -11,8 +13,9 @@ entry2 = tk.Entry(root)
 entry1.grid(row=0, column= 1)
 entry2.grid(row=1, column= 1)
 
-
+button = tk.Button(root, text = "Submit", command= root.destroy)
+button.grid(row=4, column=0)
 
 root.mainloop()
-print(entry1)
-print(entry2)
+print(entry1.get)
+print(entry2.get)
