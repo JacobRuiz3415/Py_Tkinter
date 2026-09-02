c41e4aff4bdf0d2c9290326a22a255da43ce499c
JacobRuiz3415 <jacobruiz428@gmail.com>
2026-09-01 21:16:22 -0700

    created the question checking

--- question.py
+++ question.py
@@ -1,13 +1,21 @@
 import tkinter as tk
 
 root = tk.Tk()
-v = tk.StringVar()
+v = tk.StringVar(value=1)
 
 def checkAwnswer():
-    print(f"{v.get()}")
+    answer = "2"
+    if(v.get() == answer):
+        print("correct")
+    else:
+        print("wrong")
 
-tk.Radiobutton(root, text="A", variable=v, value=1).pack(anchor=tk.W)
-tk.Radiobutton(root, text="B", variable=v, value=2).pack(anchor=tk.W)
+
+label = tk.Label(root, text="what is two plus two")
+label.pack()
+
+tk.Radiobutton(root, text="3", variable=v, value=1).pack(anchor=tk.W)
+tk.Radiobutton(root, text="4", variable=v, value=2).pack(anchor=tk.W)
 
 #get value from button
 btn = tk.Button(root, text="Submit", command=checkAwnswer)
