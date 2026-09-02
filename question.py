e7a4fa3ab2afe0fe17c8648fcce830a30488159f
JacobRuiz3415 <jacobruiz428@gmail.com>
2026-09-01 21:08:59 -0700

    get value from radio buttons

--- question.py
+++ question.py
@@ -1,8 +1,16 @@
 import tkinter as tk
 
 root = tk.Tk()
-v = tk.IntVar()
+v = tk.StringVar()
 
-tk.Radiobutton(root, text="A", variable=v, value=1).pack(anchor=tk.w)
-tk.Radiobutton(root, text="B", variable=v, value=2).pack(anchor=tk.w)
+def checkAwnswer():
+    print(f"{v.get()}")
+
+tk.Radiobutton(root, text="A", variable=v, value=1).pack(anchor=tk.W)
+tk.Radiobutton(root, text="B", variable=v, value=2).pack(anchor=tk.W)
+
+#get value from button
+btn = tk.Button(root, text="Submit", command=checkAwnswer)
+btn.pack()
+
 root.mainloop()
