1abc65aad1d85e19fc3dbfb52f3c5ca491687909
JacobRuiz3415 <jacobruiz428@gmail.com>
2026-09-06 22:23:47 -0700

    working on paint app

--- PaintingApp.py
+++ PaintingApp.py
@@ -2,3 +2,15 @@ from tkinter import *
 root = TK()
 
 root.title("Paint App")
+root.geometry("500x500")
+
+def paint(event):
+    x1, y1 = (event.x - 1), (event.y - 1)
+    x2, y2 = (event.x + 1), (event.y + 1)
+
+    color = "black"
+
+    w.create_line(x1, y1, x2, y2, fill=color)
+
+    w = Canvas(root, width=500, height=500)
+    
