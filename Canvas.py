e11396c6fb064d41337993f62520719ea6de238c
JacobRuiz3415 <jacobruiz428@gmail.com>
2026-09-06 22:11:25 -0700

    smile

--- Canvas.py
+++ Canvas.py
@@ -7,6 +7,9 @@ C = Canvas(root, bg = "yellow", height=250, width=300)
 
 line = C.create_line(108, 120, 320, 40, fill="green")
 
+oval = C.create_oval(50, 50, 100, 100, fill="blue")
+oval = C.create_oval(50, 50, 10, 10, fill="black")
+oval = C.create_oval(80, 50, 10, 10, fill="black")
 
 
 C.pack()
